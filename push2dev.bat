55d9c0d1107a0a2e5522e2f54a59ac55bbe8329c
mdebacker <mike.debacker@chrysalis.net>
2018-11-20 14:16:42 -0800

    testing screen-pop issues in classic mode

--- push2dev.bat
+++ push2dev.bat
@@ -17,3 +17,4 @@ REM
 echo Deploying metadata...
 call sfdx force:mdapi:deploy -u WW25 -d mdapioutput/ -w 100
 rem  sfdx force:mdapi:deploy -u whitewalker -d mdapioutput/ -w 100
+rem  sfdx force:mdapi:deploy -u SvcConTest -d mdapioutput/ -w 100
